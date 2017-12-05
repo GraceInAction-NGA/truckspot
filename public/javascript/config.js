5ae5783a12fac77a2f943c03a885ee2443a391fe
Orit Szwarcman <oritszwarcman@Orits-MacBook-Air.local>
2017-12-05 10:26:56 -0500

    added functionality for recording music and video and firebase integration

---
+++ public/javascript/config.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyA9ZEazOIgNSLetEEofe87l7TDOvgLWgls",
+    authDomain: "storagetest-3c2eb.firebaseapp.com",
+    databaseURL: "https://storagetest-3c2eb.firebaseio.com",
+    projectId: "storagetest-3c2eb",
+    storageBucket: "storagetest-3c2eb.appspot.com",
+    messagingSenderId: "1003649997549"
+  };
+  firebase.initializeApp(config);
