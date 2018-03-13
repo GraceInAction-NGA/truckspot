55d359207fa43ed9a6e270520113eabdf9084cdb
jess93730830 <jessicara@graceinactiondetroit.org>
2018-03-13 18:20:51 -0400

    We changed the button clicks

---
+++ public/javascript/button.js
@@ -0,0 +1,26 @@
+
+function onBtnVideoClicked (){
+
+	window.location="truckidle.html";
+}
+
+function onBtnAudioClicked() {
+
+	window.location="trucksound.html";
+}
+function onBtnVideoRecordClicked (){
+
+	window.location="video.html";
+}
+function onBtnVideoFormSubmitClicked (){
+
+	window.location="idle-data.html";
+}
+function onBtnAudioRecordClicked (){
+
+	window.location="audio.html";
+}
+function onBtnDataClicked (){
+
+	window.location="data.html";
+}
