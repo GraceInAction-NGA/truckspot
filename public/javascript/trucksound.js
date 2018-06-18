71fd539e4c3a05ab96d9df18e0c8fc3604345b84
seancote <sccote@gmail.com>
2018-06-18 14:38:18 -0400

    fixed mobile nav, form updates, misc fixes

--- public/javascript/trucksound.js
+++ public/javascript/trucksound.js
@@ -1,4 +1,4 @@
-function onSubmitClicked() {
+function onBtnAudioFormSubmitClicked() {
 	var name=document.getElementById('name').value;
 	console.log(name);
 
@@ -8,9 +8,12 @@ function onSubmitClicked() {
 	var duration=document.getElementById('duration').value;
 	console.log(duration);
 
-	var nearestaddress=document.getElementById('nearestaddress').value;
-	console.log(nearestaddress);
+	var nearestAddress=document.getElementById('nearestAddress').value;
+	console.log(nearestAddress);
 
 	var description=document.getElementById('description').value;
 	console.log(description);
+
+	var audioMedia=document.getElementById('audioMedia').value;
+	console.log(audioMedia);
 }
