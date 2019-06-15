497a528c6661e2b618077fa2e94dc2d76dcb65d4
luis <luisgonzalezyante@gmail.com>
2019-06-15 10:23:08 -0400

    Fractored download_filter/fixed bug that occurred when you submited with nothing selected

--- temp/js/download_filter.js
+++ temp/js/download_filter.js
@@ -1,114 +1,28 @@
 // filter function
 
 $(function() {
-  $("#filterSubmit").click(() => {
-    filter();
-  })
+  $("#filterSubmit").click(filter);
 })
 
+function filter() {
+  const first = $.map($("#first input:checked"), e => $(e).val());  
+  const second = $.map($("#second input:checked"), e => $(e).val());  
+  const third = $.map($("#third input:checked"), e => $(e).val());  
+  const fourth = $.map($("#fourth input:checked"), e => $(e).val());  
+  
+  let query = database.collection("reports");
 
-function filter(query) {
-  // Clear out old reports
-  $(".reports").empty();
+  query = modifyQuery(query, first, "city");
+  query = modifyQuery(query, second, "duration_range");
+  query = modifyQuery(query, third, "truck_amount");
+  query = modifyQuery(query, fourth)
 
-
-var data = [];
-  $.each($("#first input:checked"), function(){
-    data.push($(this).val());
-});
-
-if (data.length === 0)
-  data.push("all");
-
-  $.each($("#second input:checked"), function(){
-    data.push($(this).val());
-});
-
-  if (data.length === 1)
-  data.push("all");
-
-  $.each($("#third input:checked"), function(){
-    data.push($(this).val());
-});
-
-  if (data.length === 2)
-  data.push("all");
-
-  $.each($("#fourth input:checked"), function(){
-    data.push($(this).val());
-});
-
-  if (data.length === 3)
-  data.push("all");
-
-console.log(data);
-  let initialQuery = database.collection("reports");
-
-    if (data[0] !== "all") {
-      initialQuery = database.collection("reports").where("city", "==", data[0])
-    }
-
-    if (data[1] !== "all") {
-      initialQuery = initialQuery.where("duration_range", "==", data[1]);
-    }
-
-    if (data[2] !== "all") {
-      initialQuery = initialQuery.where("truck_amount", "==", data[2]);
-    }
-
-    if (data[3] !== "all") {
-      initialQuery = initialQuery.where(data[3], "==", data[3])
-
-      if(data.length === 5) {
-        initialQuery=initialQuery.where(data[4], "==", data[4])
-      }
-
-      if(data.length === 6){
-        initialQuery=initialQuery.where(data[5], "==", data[5])
-      }
-
-      if(data.length === 7){
-        initialQuery=initialQuery.where(data[6], "==", data[6])
-      }
-
-    }
-  let localQuery = initialQuery;
-
-  database.collection("counts").get().then((queryCount) => {
-    masterCount = queryCount.docs[0].data().count;
-
-    localQuery.get().then((querySnapshot) => {
-      $("#loading").hide();
-
-      console.log(querySnapshot.docs);
-
-      // Set Pagination
-      if (querySnapshot.docs.length > 0) {
-        const lastEntry = querySnapshot.docs[querySnapshot.docs.length - 1];
-        setNextPagination(lastEntry);
-        setPrevPagination(lastEntry);
-      }
-
-      querySnapshot.forEach(doc => createReportEntry(doc));
-
-      bindReportsDataToModal();
-
-      // Update Pagination
-      var pages = Math.round(masterCount / limit);
-      var page = Math.round(count / limit);
-      $(".pagination").text(page + " out of " + pages);
-
-      // Disables Last Button
-      if (count <= limit) {
-        $("#lastBtn").attr("disabled", "disabled");
-      }
-
-      // Update Next Button
-      if (count >= masterCount) {
-        $("#nextBtn").attr("disabled", "disabled");
-      } else {
-        $("#nextBtn").removeAttr("disabled");
-      }
-    });
-  });
+  download(query, true);
 };
+
+function modifyQuery(query, data, key) {
+  hasValidQuery = data.length > 0 && !data.includes("all");
+  const reducer = (acc, queryKey) => acc.where(key ? key : queryKey, "==", queryKey); 
+
+  return hasValidQuery ? data.reduce(reducer, query) : query;
+}
